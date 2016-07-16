f0f33ada4ffad89ea57ac824c5a17e16ceb4cec7
Ian Stewart <istewart@yelp.com>
2016-07-16 14:40:20 -0700

    Mostly filled in parser.

--- src/parser/ParserV1.java
+++ src/parser/ParserV1.java
@@ -34,7 +34,7 @@ public class ParserV1 implements Parser {
 			String symbol = split[0];
 			int position = Integer.parseInt(split[1]);
 
-			book.setEquity(symbol, position);
+			book.update(symbol, position);
 		}
 	}
 
@@ -53,9 +53,69 @@ public class ParserV1 implements Parser {
 	}
 
 	public void parseBook(String[] split) {
-		for (int i = 1; i < split.size; i++) {
+		String symbol = split[1];
+		boolean isBuy = true;
+
+		List<Array<Integer>> buy = new ArrayList<>();
+		List<Array<Integer>> sell = new ArrayList<>();
+
+		for (int i = 2; i < split.size; i++) {
 			String curr = split[i];
-			if ()
+			if (curr.equals("BUY")) {
+				isBuy = true;
+			} else if (curr.equals("SELL")) {
+				isBuy = false;
+			} else {
+				String[] currSplit = curr.split(":");
+
+				int price = Integer.parseInt(currSplit[0]);
+				int size = Integer.parseInt(currSplit[1]);
+
+				if (isBuy) {
+					buy.add({price, size});
+				} else {
+					sell.add({price, size});
+				}
+			}
 		}
+
+		book.updateBuy(symbol, buy);
+		book.updateSell(symbol, sell);
+	}
+
+	public void parseTrade(String[] split) {
+		String symbol = split[1];
+		int price = Integer.parseInt(split[2]);
+		int size = Integer.parseInt(split[3]);
+
+		book.trade(symbol, price, size);
+	}
+
+	public void parseAck(String[] split) {
+		int orderID = Integer.parseInt(split[1]);
+		book.updateOrder(orderID, "ACK");
+	}
+
+	public void parseReject(String[] split) {
+		int orderID = Integer.parseInt(split[1]);
+		String reason = split[2];
+
+		System.out.println("ORDER " + orderID + "REJECTED: " + reason);
+		book.updateOrder(orderID, "REJECTED");
+	}
+
+	public void parseFill(String[] split) {
+		int orderID = Integer.parseInt(split[1]);
+		String symbol = split[2];
+		String type = split[3];
+		int price = Integer.parseInt(split[4]);
+		int size = Integer.parseSize(split[5]);
+
+		book.fillOrder(orderID, symbol, type, price, size);
+	}
+
+	public void parseOut(String[] split) {
+		int orderID = Integer.parseInt(split[1]);
+		book.updateOrder(orderID, "OUT");
 	}
 }
