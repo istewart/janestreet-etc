f28ec679b5f71b1b29ef5d4d2b665e43319afafc
danwang1995 <danwang1995@gmail.com>
2016-07-16 20:20:44 -0700

    More bugfixes

--- BookV1.java
+++ BookV1.java
@@ -23,13 +23,13 @@ public class BookV1 implements Book {
     public void fill(int orderId, String s, String type, int price, int size) {
         if (type.equals("BUY")) {
             if (ourBuys.containsKey(s)) {
-                ourBuys.put(s, ourBuys.get(s, 0) - size);
+                ourBuys.put(s, ourBuys.get(s) - size);
             } else {
                 ourBuys.put(s, -size);
             }
             
             if (positions.containsKey(s)) {
-                positions.put(s, positions.get(s, 0) + size);
+                positions.put(s, positions.get(s) + size);
             } else {
                 positions.put(s, size);
             }
@@ -37,7 +37,7 @@ public class BookV1 implements Book {
             cash -= price * size;
         } else if (type.equals("SELL")) {
             if (ourSells.containsKey(s)) {
-                ourSells.put(s, ourSells.get(s, 0) - size);
+                ourSells.put(s, ourSells.get(s) - size);
             } else {
                 ourSells.put(s, -size);
             }
@@ -50,22 +50,23 @@ public class BookV1 implements Book {
         OrderV1 order = orders.get(orderId);
         String s = order.getName();
         int size = order.getAmount();
+        String type = order.getType();
         
         if (type.equals("BUY")) {
             if (ourBuys.containsKey(s)) {
-                ourBuys.put(s, ourBuys.get(s, 0) + size);
+                ourBuys.put(s, ourBuys.get(s) + size);
             } else {
                 ourBuys.put(s, size);
             }
         } else if (type.equals("SELL")) {
             if (ourSells.containsKey(s)) {
-                ourSells.put(s, ourSells.get(s, 0) + size);
+                ourSells.put(s, ourSells.get(s) + size);
             } else {
                 ourSells.put(s, size);
             }
             
             if (positions.containsKey(s)) {
-                positions.put(s, positions.get(s, 0) - size);
+                positions.put(s, positions.get(s) - size);
             } else {
                 positions.put(s, -size);
             }
